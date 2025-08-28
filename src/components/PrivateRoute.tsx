b4fe039066c0e0b0c990eee8843f4d88a809e143
thighnurger <lewis177777gmail.com>
2025-08-28 23:09:40 +0900

    feat: 로그인,학생인증 여부에 따른 라우팅 구현

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from 'react-router';
+import useAuthStore from '@/stores/authStore';
+
+export const PrivateRoute = () => {
+  const { isAuthenticated, verificationStatus } = useAuthStore();
+
+  //로그인이 되어있지 않은경우
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  //학생인증이 되어있지 않은 경우
+  if (verificationStatus === 'unverified' || verificationStatus === 'pending') {
+    return <Navigate to="/student" replace />;
+  }
+
+  //로그인이 되었고 학생 인증이 완료된 경우
+  return <Outlet />;
+};
