5cfa1f4f066d62340e0efead4d02c23cbe5c3f8d
thighnurger <lewis177777gmail.com>
2025-08-28 23:07:54 +0900

    set: BaseLayout으로 Routes감쌈

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import GlobalStyle from './styles/GlobalStyle';
 import { Routes } from './pages/Routes';
+import { BaseLayout } from './Layout/BaseLayout';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Routes />
+      <BaseLayout>
+        <Routes />
+      </BaseLayout>
     </>
   );
 }
