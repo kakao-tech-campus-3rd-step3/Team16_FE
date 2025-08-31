79957de45e2d9bd5faeaa200fdd5b9ab07dc7799
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-09-01 02:06:44 +0900

    design: SearchField css 일부 수정
    마진 간격, 검색창 높이 조절함.

--- src/pages/Home/components/SearchFieldSection/index.tsx
+++ src/pages/Home/components/SearchFieldSection/index.tsx
@@ -13,17 +13,16 @@ const SearchFieldSection = () => {
 export default SearchFieldSection;
 
 const Wrapper = styled.main(({ theme }) => ({
-  margin: `0 ${theme.spacing.spacing4}px`,
+  margin: `${theme.spacing.spacing3}px ${theme.spacing.spacing4}px`,
   display: 'flex',
   flexDirection: 'row',
-  marginTop: theme.spacing.spacing5,
   alignItems: 'center',
-  gap: theme.spacing.spacing4,
+  gap: theme.spacing.spacing2,
 }));
 
 const SearchField = styled.input(({ theme }) => ({
   width: '100%',
-  height: '40px',
+  height: '36px',
   borderRadius: '16px',
   border: `2px solid ${theme.colors.gray300}`,
   backgroundColor: theme.colors.gray100,
