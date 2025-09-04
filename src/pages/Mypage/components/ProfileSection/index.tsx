15e103ceb8aec02369445d06721dc87086d3af97
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-09-05 01:55:59 +0900

    design: ProfileSection css 수정

--- src/pages/Mypage/components/ProfileSection/index.tsx
+++ src/pages/Mypage/components/ProfileSection/index.tsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled';
 import { IoSettingsOutline } from 'react-icons/io5';
+import { colors } from '@/styles/colors';
+import { typography } from '@/styles/typography';
+import { spacing } from '@/styles/spacing';
 
 const ProfileSection = () => {
   return (
@@ -18,44 +21,44 @@ const ProfileSection = () => {
 
 export default ProfileSection;
 
-const Wrapper = styled.main(({ theme }) => ({
-  margin: `0px ${theme.spacing.spacing4}px`,
-  padding: `${theme.spacing.spacing4}px 0px`,
+const Wrapper = styled.section({
+  margin: `0px ${spacing.spacing4}px`,
+  padding: `${spacing.spacing4}px 0px`,
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
-  gap: theme.spacing.spacing2,
-  backgroundColor: theme.colors.gray50,
-}));
+  gap: spacing.spacing2,
+  backgroundColor: colors.backgroundGray,
+});
 
-const ProfileImage = styled.image(({ theme }) => ({
+const ProfileImage = styled.image({
   width: '64px',
   height: '64px',
   borderRadius: '50%',
-  backgroundColor: theme.colors.gray300, //임시
-}));
+  backgroundColor: colors.gray300, //임시
+});
 
-const ProfileInfo = styled.div(({ theme }) => ({
+const ProfileInfo = styled.div({
   display: 'flex',
   flexDirection: 'column',
-  gap: theme.spacing.spacing1,
+  gap: spacing.spacing1,
   flex: 1,
-}));
+});
 
-const Nickname = styled.span(({ theme }) => ({
-  ...theme.typography.h1,
-}));
+const Nickname = styled.span({
+  ...typography.h1,
+});
 
-const SettingButton = styled.button(({ theme }) => ({
+const SettingButton = styled.button({
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
-  gap: theme.spacing.spacing1,
-  backgroundColor: theme.colors.gray200,
+  gap: spacing.spacing1,
+  backgroundColor: colors.gray200,
   borderRadius: '24px',
   border: 'none',
   padding: '8px 12px',
   cursor: 'pointer',
-  ...theme.typography.body,
-  color: theme.colors.black,
-}));
+  ...typography.body,
+  color: colors.black,
+});
