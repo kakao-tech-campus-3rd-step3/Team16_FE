7ac3cc411a5a78f562293b0a527d0fdeae50c6b1
thighburger <lewis177777@gmail.com>
2025-09-09 13:52:39 +0900

    feat: axios onUploadProgress 를 이용한 업로드 진행률 추적 기능 구현

--- src/hooks/useImageUpload.ts
+++ src/hooks/useImageUpload.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { uploadImageApi } from '@/api/imageUploader';
+import { useState } from 'react';
 
 interface UseImageUploadOptions {
   uploadUrl: string; // 이미지 업로드용 URL
@@ -8,25 +9,30 @@ interface UseImageUploadOptions {
 
 export const useImageUpload = (options: UseImageUploadOptions) => {
   const { uploadUrl, completionUrl } = options;
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
 
   const { mutate, isPending } = useMutation<string, Error, File>({
     mutationFn: (file: File) =>
       uploadImageApi(file, {
         uploadUrl,
         completionUrl,
+        onProgress: (progress) => setUploadProgress(progress),
       }),
 
     onSuccess: () => {
       alert('제출이 완료되었습니다!');
+      setUploadProgress(0);
     },
 
     onError: () => {
       alert('제출 중 오류가 발생했습니다.');
+      setUploadProgress(0);
     },
   });
 
   return {
     uploadImage: mutate,
     isUploading: isPending,
+    uploadProgress,
   };
 };
