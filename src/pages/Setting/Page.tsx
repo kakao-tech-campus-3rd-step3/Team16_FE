c031ef8eaa4eef7871b68e5737134c8dd67f4153
thighburger <lewis177777@gmail.com>
2025-09-10 17:39:26 +0900

    feat: 설정 ui, 유저프로필 fetch

---
+++ src/pages/Setting/Page.tsx
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+import { colors } from '@/styles/colors';
+import { typography } from '@/styles/typography';
+import { useUserProfile } from '@/hooks/useUserProfile';
+
+const options = [
+  { label: '프로필 수정', action: 'editProfile' },
+  { label: '로그아웃', action: 'logout' },
+  { label: '회원탈퇴', action: 'deleteAccount' },
+];
+
+const SettingPage = () => {
+  const { profileImg, nickname, isLoading } = useUserProfile();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Wrapper>
+      <ProfileSection>
+        <ProfileImg src={profileImg} />
+        <Nickname>{nickname}</Nickname>
+      </ProfileSection>
+
+      {options.map((option) => (
+        <Option key={option.label}>
+          <span style={{ paddingLeft: '15px' }}>{option.label}</span>
+        </Option>
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div({});
+
+const ProfileSection = styled.div({
+  height: '250px',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  gap: '20px',
+  borderBottom: '1px solid #eee',
+});
+
+const ProfileImg = styled.img({
+  width: '150px',
+  height: '150px',
+  borderRadius: '50%',
+  marginTop: '30px',
+  objectFit: 'cover',
+});
+
+const Nickname = styled.div({
+  ...typography.h1,
+  color: colors.black,
+  marginBottom: '20px',
+});
+
+const Option = styled.div({
+  ...typography.body,
+  justifyContent: 'center',
+  padding: '20px 0',
+  borderBottom: '1px solid #eee',
+});
+
+export default SettingPage;
