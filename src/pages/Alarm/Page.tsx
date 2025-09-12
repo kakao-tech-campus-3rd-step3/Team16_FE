1e021cec10de201331114e40c32a3b84af1a1754
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-09-12 16:13:22 +0900

    feat: 알림 리스트 UI

---
+++ src/pages/Alarm/Page.tsx
@@ -0,0 +1,31 @@
+import BottomNavigation from '@/components/common/BottomNavigation';
+import { useHeader } from '@/hooks/useHeader';
+import AlarmList from './components/AlarmList';
+import styled from '@emotion/styled';
+
+const AlarmPage = () => {
+  useHeader({ center: '알림' });
+  return (
+    <PageContainer>
+      <ListContainer>
+        <AlarmList />
+      </ListContainer>
+      <BottomNavigation />
+    </PageContainer>
+  );
+};
+
+export default AlarmPage;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const ListContainer = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  min-height: 0;
+  background-color: white;
+`;
