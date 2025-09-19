f880dc8b46a4b92731afa879cb1cab4b2e7e07f4
thighburger <lewis177777@gmail.com>
2025-09-19 21:59:32 +0900

    refact: 시간 상수 정의

--- src/pages/Group/components/DateDiff.tsx
+++ src/pages/Group/components/DateDiff.tsx
@@ -2,6 +2,11 @@ import styled from '@emotion/styled';
 import { colors } from '@/styles/colors';
 import { typography } from '@/styles/typography';
 
+const SEC_MS = 1000;
+const MIN_MS = SEC_MS * 60;
+const HOUR_MS = MIN_MS * 60;
+const DAY_MS = HOUR_MS * 24;
+
 const DateDiff = ({ date }: { date: string }) => {
   const text = calDateDiff(date);
   return <Wrapper text={text}>{text}</Wrapper>;
@@ -11,9 +16,9 @@ const calDateDiff = (date: string) => {
   const now = new Date();
   const target = new Date(date);
   const diffMs = target.getTime() - now.getTime();
-  const diffMin = Math.floor(diffMs / 1000 / 60);
-  const diffHour = Math.floor(diffMin / 60);
-  const diffDay = Math.floor(diffHour / 24);
+  const diffMin = Math.floor(diffMs / MIN_MS);
+  const diffHour = Math.floor(diffMs / HOUR_MS);
+  const diffDay = Math.floor(diffMs / DAY_MS);
   let text = '';
   if (diffDay >= 1) {
     text = `${diffDay}일 전`;
