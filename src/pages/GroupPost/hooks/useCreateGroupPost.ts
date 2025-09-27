f9ff5ba391a3667f840b94e4317aef50da7eb7d4
thighnurger <lewis177777gmail.com>
2025-09-27 21:02:28 +0900

    feat: 게시판 업로드 훅

---
+++ src/pages/GroupPost/hooks/useCreateGroupPost.ts
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query';
+import { createGroupPostApi } from '@/pages/GroupPost/api/createGroupPostApi';
+import type { GroupPostFormData } from '@/pages/GroupPost/type';
+
+export const useCreateGroupPost = (groupId: number) => {
+  const mutation = useMutation<void, unknown, GroupPostFormData>({
+    mutationFn: (data: GroupPostFormData) =>
+      createGroupPostApi({
+        ...data,
+        groupId: Number(groupId),
+      }),
+  });
+
+  const { mutateAsync } = mutation;
+
+  return {
+    createGroupPost: mutateAsync,
+  };
+};
+
+export default useCreateGroupPost;
