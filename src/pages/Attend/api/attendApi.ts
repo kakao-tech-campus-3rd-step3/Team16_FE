336b8b36747fec6cb1a00bf90f11c871fb85cbfb
thighburger <lewis177777@gmail.com>
2025-10-02 18:04:18 +0900

    feat: 출석 조회 api

--- src/pages/Attend/api/attendApi.ts
+++ src/pages/Attend/api/attendApi.ts
@@ -2,6 +2,5 @@ import { apiClient } from '@/api/apiClient';
 
 export const getUserAttendanceStatus = async (groupId: number, planId: number) => {
   const response = await apiClient.get(`/groups/${groupId}/attends/${planId}`);
-  console.log('getUserAttendanceStatus response:', response.data);
   return response.data;
 };
