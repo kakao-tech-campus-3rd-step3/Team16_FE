a81a14afa48b43ee6528d114b94a035627b7ff65
thighburger <lewis177777@gmail.com>
2025-10-02 18:03:35 +0900

    refact: type변경

--- src/hooks/useGroundRules.ts
+++ src/hooks/useGroundRules.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchGroundRules } from '@/api/groupApi';
 
-export const useGroundRules = (groupId: string) => {
+export const useGroundRules = (groupId: number) => {
   return useQuery({
     queryKey: ['groundRules', groupId],
     queryFn: () => fetchGroundRules(Number(groupId)),
