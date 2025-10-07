46e988618569ea0e4b8b565b5e813e8025914639
thighnurger <lewis177777gmail.com>
2025-10-07 21:31:12 +0900

    feat:로그인 로직 수정

--- src/pages/Login/Page.tsx
+++ src/pages/Login/Page.tsx
@@ -2,58 +2,32 @@ import kakaoLoginBtn from '@/assets/kakao_login_button.svg';
 import styled from '@emotion/styled';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import useAuthStore from '@/stores/authStore';
+import { getUserInfo } from '@/api/userApi';
 
-const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
-const VITE_REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+const kakaoLoginURI = import.meta.env.VITE_KAKAO_LOGIN_URI;
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const { setAccessToken } = useAuthStore(); // zustand store에서 setAccessToken 함수 가져오기
+  const { setAccessToken, setUserInfo } = useAuthStore(); // zustand store에서 setAccessToken 함수 가져오기
 
   useEffect(() => {
-    // URL에서 인가코드 확인
-    const code = new URL(window.location.href).searchParams.get('code');
-
-    if (code) {
-      console.log('인가코드 받음:', code);
-      handleKakaoCallback(code);
+    const accessToken = new URL(window.location.href).searchParams.get('token');
+    if (accessToken) {
+      setAccessToken(accessToken);
+      console.log(accessToken);
+      // 사용자 정보 가져오고 유저정보 zustand에 저장
+      getUserInfo().then((userInfo) => {
+        console.log(userInfo);
+        setUserInfo(userInfo);
+        navigate('/'); // 메인 페이지로 이동
+      });
     }
   }, []);
 
-  const handleKakaoCallback = async (code: string) => {
-    try {
-      // axios로 백엔드 API에 인가코드 전송
-      const response = await axios.post('/api/auth/kakao-login', {
-        code,
-      });
-
-      const result = response.data;
-
-      // 로그인 성공 - zustand store에 액세스 토큰 저장
-      setAccessToken(result.accessToken);
-
-      navigate('/');
-    } catch (error: any) {
-      console.error('카카오 로그인 처리 에러:', error);
-
-      // axios 에러 처리
-      if (error.response) {
-        // 서버에서 에러 응답을 받은 경우
-        const errorMessage = error.response.data?.message || '로그인 처리 실패';
-        alert(errorMessage);
-      } else {
-        // 네트워크 에러 등
-        alert('로그인 처리 중 오류가 발생했습니다.');
-      }
-    }
-  };
-
-  // oauth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&VITE_REDIRECT_URI=${VITE_REDIRECT_URI}&response_type=code`;
   const handleLogin = () => {
-    window.location.href = kakaoURL;
+    // 카카오 로그인 페이지로 리다이렉트
+    window.location.href = kakaoLoginURI;
   };
   return (
     <>
