30a0bb9bac14fc4ad0ca590a4c5072e54c5ba6d8
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-10-09 23:03:24 +0900

    fix: 리뷰작성, 탈퇴 순서 수정

--- src/hooks/useLeave.ts
+++ src/hooks/useLeave.ts
@@ -1,12 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
-// import { postGroupReview } from '@/api/leaveApi';
+import { postGroupReview } from '@/api/leaveApi';
 import { leaveGroup } from '@/api/leaveApi';
 
 export function useLeave() {
   return useMutation<void, Error, { content: string; groupId: number }>({
-    mutationFn: async ({ groupId }) => {
-      // await postGroupReview(content, groupId);
+    mutationFn: async ({ content, groupId }) => {
       await leaveGroup(groupId);
+      await postGroupReview(content, groupId);
     },
   });
 }
