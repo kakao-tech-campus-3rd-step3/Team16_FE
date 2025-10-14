9e94a2c25d533cdca744832aef961e3477adce20
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-10-15 00:24:23 +0900

    feat: 참가신청목록 api 정의, 그룹api와 분리함

---
+++ src/api/applicantsApi.ts
@@ -0,0 +1,11 @@
+import { apiClient } from './apiClient';
+
+export const fetchGroupApplications = async (groupId: number) => {
+  const res = await apiClient.get(`/group/${groupId}/join-requests`);
+  return res.data;
+};
+
+export const approveGroupApplication = async (groupId: number, userId: number) => {
+  const res = await apiClient.post(`/group/join`, { groupId, userId });
+  return res.data;
+};
