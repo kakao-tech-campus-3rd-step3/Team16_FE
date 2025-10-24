fe039c95f8214c7590a8cad60875d558d549273a
jiiiwonyy <119236218+jiiiwonyy@users.noreply.github.com>
2025-10-24 14:51:04 +0900

    fix: 리뷰섹션 api연동돼서 주석제거

--- src/pages/Mypage/Page.tsx
+++ src/pages/Mypage/Page.tsx
@@ -1,7 +1,7 @@
 import BottomNavigation from '@/components/common/BottomNavigation';
 import ProfileSection from './components/ProfileSection';
 import RecordSection from './components/RecordSection';
-// import ReviewSection from './components/ReviewSection';
+import ReviewSection from './components/ReviewSection';
 import styled from '@emotion/styled';
 import { colors } from '@/styles/colors';
 import { useParams } from 'react-router-dom';
@@ -16,7 +16,7 @@ const Mypage = () => {
     <Wrapper>
       <ProfileSection userId={targetId} isMyPage={isMyPage} />
       <RecordSection userId={targetId} />
-      {/* <ReviewSection userId={targetId} /> */}
+      <ReviewSection userId={targetId} />
       {isMyPage && <BottomNavigation />}
     </Wrapper>
   );
